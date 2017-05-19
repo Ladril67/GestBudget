958a7c11a7926ae9389232a58e93b02192d8ad39
Ladril67 <ladril67@hotmail.fr>
2017-05-19 10:21:09 +0200

    ajout de code simple

--- GestBudget/frmBudgetMois.cs
+++ GestBudget/frmBudgetMois.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace GestBudget
 {
     public partial class frmBudgetMois : Form
     {
+        OleDbConnection connec = new OleDbConnection();
+        DataSet ds = new DataSet();
+
         public frmBudgetMois()
         {
             InitializeComponent();
@@ -19,7 +23,41 @@ namespace GestBudget
 
         private void frmBudgetMois_Load(object sender, EventArgs e)
         {
+            try
+            {
+                connec.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\ladri\DUT\A21\MINI_PROJET\GestBudget\budget.mdb";
+                connec.Open();
+                DataTable schema = connec.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                connec.Close();
 
+                string requete = "";
+                string nomTable = "";
+
+                foreach (DataRow ligne in schema.Rows)
+                {
+                    nomTable = ligne[2].ToString();
+                    requete = @"select * from " + nomTable;
+                    OleDbCommand cd1 = new OleDbCommand(requete, connec);
+                    OleDbDataAdapter da = new OleDbDataAdapter();
+                    da.SelectCommand = cd1;
+
+
+                    da.Fill(ds, nomTable);
+
+                }
+
+            }
+            catch (InvalidOperationException erreur)
+            {
+                MessageBox.Show("Erreur de chaine de connexion !");
+            }
+            catch (OleDbException erreur)
+            {
+                MessageBox.Show("Erreur de requete SQL !");
+            }
+            finally
+            {
+            }
         }
     }
 }
