30ce647c8fa0bb0c646b47b7b56673f089d7e33d
miniyeti67 <Miniyeti67*>
2017-05-22 12:12:01 +0200

    interface_corriger

--- GestBudget/frmBudgetMois.Designer.cs
+++ GestBudget/frmBudgetMois.Designer.cs
@@ -33,8 +33,6 @@
             this.tpAffichage1a1 = new System.Windows.Forms.TabPage();
             this.tpNewTransaction = new System.Windows.Forms.TabPage();
             this.grpRecettePercu = new System.Windows.Forms.GroupBox();
-            this.rdbPerçu = new System.Windows.Forms.RadioButton();
-            this.rdbRecette = new System.Windows.Forms.RadioButton();
             this.btnAjouterPersonneTransa = new System.Windows.Forms.Button();
             this.grpParticipantsTransa = new System.Windows.Forms.GroupBox();
             this.btnAjoutTransa = new System.Windows.Forms.Button();
@@ -50,6 +48,7 @@
             this.tpDeleteTransaction = new System.Windows.Forms.TabPage();
             this.tpModifTransaction = new System.Windows.Forms.TabPage();
             this.tpRecap = new System.Windows.Forms.TabPage();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.tcBudgetMois.SuspendLayout();
             this.tpNewTransaction.SuspendLayout();
             this.grpRecettePercu.SuspendLayout();
@@ -68,16 +67,16 @@
             this.tcBudgetMois.Margin = new System.Windows.Forms.Padding(2);
             this.tcBudgetMois.Name = "tcBudgetMois";
             this.tcBudgetMois.SelectedIndex = 0;
-            this.tcBudgetMois.Size = new System.Drawing.Size(925, 514);
+            this.tcBudgetMois.Size = new System.Drawing.Size(694, 418);
             this.tcBudgetMois.TabIndex = 0;
             // 
             // tpAffichage1a1
             // 
-            this.tpAffichage1a1.Location = new System.Drawing.Point(4, 28);
-            this.tpAffichage1a1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tpAffichage1a1.Location = new System.Drawing.Point(4, 25);
+            this.tpAffichage1a1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tpAffichage1a1.Name = "tpAffichage1a1";
-            this.tpAffichage1a1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tpAffichage1a1.Size = new System.Drawing.Size(917, 482);
+            this.tpAffichage1a1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tpAffichage1a1.Size = new System.Drawing.Size(686, 389);
             this.tpAffichage1a1.TabIndex = 1;
             this.tpAffichage1a1.Text = "Affichage 1 à 1";
             this.tpAffichage1a1.UseVisualStyleBackColor = true;
@@ -97,66 +96,40 @@
             this.tpNewTransaction.Controls.Add(this.lblDescripionTransa);
             this.tpNewTransaction.Controls.Add(this.dtpTransa);
             this.tpNewTransaction.Controls.Add(this.lblDateTransa);
-            this.tpNewTransaction.Location = new System.Drawing.Point(4, 28);
+            this.tpNewTransaction.Location = new System.Drawing.Point(4, 25);
             this.tpNewTransaction.Margin = new System.Windows.Forms.Padding(2);
             this.tpNewTransaction.Name = "tpNewTransaction";
             this.tpNewTransaction.Padding = new System.Windows.Forms.Padding(2);
-            this.tpNewTransaction.Size = new System.Drawing.Size(917, 482);
+            this.tpNewTransaction.Size = new System.Drawing.Size(686, 389);
             this.tpNewTransaction.TabIndex = 0;
             this.tpNewTransaction.Text = "Ajout d\'une transaction";
             this.tpNewTransaction.UseVisualStyleBackColor = true;
             // 
             // grpRecettePercu
             // 
-            this.grpRecettePercu.Controls.Add(this.rdbPerçu);
-            this.grpRecettePercu.Controls.Add(this.rdbRecette);
-            this.grpRecettePercu.Location = new System.Drawing.Point(20, 156);
+            this.grpRecettePercu.Controls.Add(this.radioButton1);
+            this.grpRecettePercu.Location = new System.Drawing.Point(36, 127);
+            this.grpRecettePercu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grpRecettePercu.Name = "grpRecettePercu";
-            this.grpRecettePercu.Size = new System.Drawing.Size(368, 47);
+            this.grpRecettePercu.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpRecettePercu.Size = new System.Drawing.Size(276, 55);
             this.grpRecettePercu.TabIndex = 12;
             this.grpRecettePercu.TabStop = false;
             // 
-            // rdbPerçu
-            // 
-            this.rdbPerçu.AutoSize = true;
-            this.rdbPerçu.Location = new System.Drawing.Point(265, 206);
-            this.rdbPerçu.Margin = new System.Windows.Forms.Padding(4);
-            this.rdbPerçu.Name = "rdbPerçu";
-            this.rdbPerçu.Size = new System.Drawing.Size(110, 21);
-            this.rdbPerçu.TabIndex = 6;
-            this.rdbPerçu.TabStop = true;
-            this.rdbPerçu.Text = "radioButton2";
-            this.rdbPerçu.UseVisualStyleBackColor = true;
-            // 
-            // rdbRecette
-            // 
-            this.rdbRecette.AutoSize = true;
-            this.rdbRecette.Location = new System.Drawing.Point(63, 206);
-            this.rdbRecette.Margin = new System.Windows.Forms.Padding(4);
-            this.rdbRecette.Name = "rdbRecette";
-            this.rdbRecette.Size = new System.Drawing.Size(110, 21);
-            this.rdbRecette.TabIndex = 5;
-            this.rdbRecette.TabStop = true;
-            this.rdbRecette.Text = "radioButton1";
-            this.rdbRecette.UseVisualStyleBackColor = true;
-            // 
             // btnAjouterPersonneTransa
             // 
-            this.btnAjouterPersonneTransa.Location = new System.Drawing.Point(590, 35);
-            this.btnAjouterPersonneTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAjouterPersonneTransa.Location = new System.Drawing.Point(442, 28);
             this.btnAjouterPersonneTransa.Name = "btnAjouterPersonneTransa";
-            this.btnAjouterPersonneTransa.Size = new System.Drawing.Size(75, 62);
+            this.btnAjouterPersonneTransa.Size = new System.Drawing.Size(56, 50);
             this.btnAjouterPersonneTransa.TabIndex = 0;
             this.btnAjouterPersonneTransa.Text = "Ajouter une personne";
             this.btnAjouterPersonneTransa.UseVisualStyleBackColor = true;
             // 
             // grpParticipantsTransa
             // 
-            this.grpParticipantsTransa.Location = new System.Drawing.Point(445, 19);
-            this.grpParticipantsTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.grpParticipantsTransa.Location = new System.Drawing.Point(334, 15);
             this.grpParticipantsTransa.Name = "grpParticipantsTransa";
-            this.grpParticipantsTransa.Padding = new System.Windows.Forms.Padding(4);
-            this.grpParticipantsTransa.Size = new System.Drawing.Size(130, 362);
+            this.grpParticipantsTransa.Size = new System.Drawing.Size(98, 294);
             this.grpParticipantsTransa.TabIndex = 11;
             this.grpParticipantsTransa.TabStop = false;
             this.grpParticipantsTransa.Text = "Participants : ";
@@ -164,10 +137,9 @@
             // 
             // btnAjoutTransa
             // 
-            this.btnAjoutTransa.Location = new System.Drawing.Point(84, 263);
-            this.btnAjoutTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAjoutTransa.Location = new System.Drawing.Point(81, 214);
             this.btnAjoutTransa.Name = "btnAjoutTransa";
-            this.btnAjoutTransa.Size = new System.Drawing.Size(214, 91);
+            this.btnAjoutTransa.Size = new System.Drawing.Size(160, 74);
             this.btnAjoutTransa.TabIndex = 10;
             this.btnAjoutTransa.Text = "Ajouter";
             this.btnAjoutTransa.UseVisualStyleBackColor = true;
@@ -175,10 +147,9 @@
             // 
             // btnAjoutType
             // 
-            this.btnAjoutType.Location = new System.Drawing.Point(326, 203);
-            this.btnAjoutType.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAjoutType.Location = new System.Drawing.Point(244, 187);
             this.btnAjoutType.Name = "btnAjoutType";
-            this.btnAjoutType.Size = new System.Drawing.Size(32, 23);
+            this.btnAjoutType.Size = new System.Drawing.Size(24, 19);
             this.btnAjoutType.TabIndex = 9;
             this.btnAjoutType.Text = "...";
             this.btnAjoutType.UseVisualStyleBackColor = true;
@@ -186,110 +157,115 @@
             // cbTypeTransa
             // 
             this.cbTypeTransa.FormattingEnabled = true;
-            this.cbTypeTransa.Location = new System.Drawing.Point(199, 203);
-            this.cbTypeTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.cbTypeTransa.Location = new System.Drawing.Point(146, 187);
             this.cbTypeTransa.Name = "cbTypeTransa";
-            this.cbTypeTransa.Size = new System.Drawing.Size(121, 24);
+            this.cbTypeTransa.Size = new System.Drawing.Size(92, 21);
             this.cbTypeTransa.TabIndex = 8;
             // 
             // lblTypeTransa
             // 
             this.lblTypeTransa.AutoSize = true;
-            this.lblTypeTransa.Location = new System.Drawing.Point(59, 257);
-            this.lblTypeTransa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTypeTransa.Location = new System.Drawing.Point(109, 187);
             this.lblTypeTransa.Name = "lblTypeTransa";
-            this.lblTypeTransa.Size = new System.Drawing.Size(40, 17);
+            this.lblTypeTransa.Size = new System.Drawing.Size(31, 13);
             this.lblTypeTransa.TabIndex = 7;
             this.lblTypeTransa.Text = "Type";
             // 
             // txtMontantTransa
             // 
-            this.txtMontantTransa.Location = new System.Drawing.Point(199, 119);
-            this.txtMontantTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMontantTransa.Location = new System.Drawing.Point(149, 97);
             this.txtMontantTransa.Name = "txtMontantTransa";
-            this.txtMontantTransa.Size = new System.Drawing.Size(100, 22);
+            this.txtMontantTransa.Size = new System.Drawing.Size(76, 20);
             this.txtMontantTransa.TabIndex = 4;
             // 
             // lblMontantTransa
             // 
             this.lblMontantTransa.AutoSize = true;
-            this.lblMontantTransa.Location = new System.Drawing.Point(44, 124);
-            this.lblMontantTransa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMontantTransa.Location = new System.Drawing.Point(33, 101);
             this.lblMontantTransa.Name = "lblMontantTransa";
-            this.lblMontantTransa.Size = new System.Drawing.Size(59, 17);
+            this.lblMontantTransa.Size = new System.Drawing.Size(46, 13);
             this.lblMontantTransa.TabIndex = 3;
             this.lblMontantTransa.Text = "Montant";
             // 
             // txtDescriptionTransa
             // 
-            this.txtDescriptionTransa.Location = new System.Drawing.Point(199, 78);
-            this.txtDescriptionTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescriptionTransa.Location = new System.Drawing.Point(149, 63);
             this.txtDescriptionTransa.Name = "txtDescriptionTransa";
-            this.txtDescriptionTransa.Size = new System.Drawing.Size(100, 22);
+            this.txtDescriptionTransa.Size = new System.Drawing.Size(76, 20);
             this.txtDescriptionTransa.TabIndex = 3;
             // 
             // lblDescripionTransa
             // 
             this.lblDescripionTransa.AutoSize = true;
-            this.lblDescripionTransa.Location = new System.Drawing.Point(44, 83);
-            this.lblDescripionTransa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescripionTransa.Location = new System.Drawing.Point(33, 67);
             this.lblDescripionTransa.Name = "lblDescripionTransa";
-            this.lblDescripionTransa.Size = new System.Drawing.Size(79, 17);
+            this.lblDescripionTransa.Size = new System.Drawing.Size(60, 13);
             this.lblDescripionTransa.TabIndex = 2;
             this.lblDescripionTransa.Text = "Description";
             // 
             // dtpTransa
             // 
-            this.dtpTransa.Location = new System.Drawing.Point(199, 35);
-            this.dtpTransa.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpTransa.Location = new System.Drawing.Point(149, 28);
             this.dtpTransa.Name = "dtpTransa";
-            this.dtpTransa.Size = new System.Drawing.Size(221, 22);
+            this.dtpTransa.Size = new System.Drawing.Size(167, 20);
             this.dtpTransa.TabIndex = 1;
             // 
             // lblDateTransa
             // 
             this.lblDateTransa.AutoSize = true;
-            this.lblDateTransa.Location = new System.Drawing.Point(44, 35);
-            this.lblDateTransa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDateTransa.Location = new System.Drawing.Point(33, 28);
             this.lblDateTransa.Name = "lblDateTransa";
-            this.lblDateTransa.Size = new System.Drawing.Size(147, 17);
+            this.lblDateTransa.Size = new System.Drawing.Size(111, 13);
             this.lblDateTransa.TabIndex = 0;
             this.lblDateTransa.Text = "Date de la transaction";
             // 
             // tpDeleteTransaction
             // 
-            this.tpDeleteTransaction.Location = new System.Drawing.Point(4, 28);
+            this.tpDeleteTransaction.Location = new System.Drawing.Point(4, 25);
             this.tpDeleteTransaction.Margin = new System.Windows.Forms.Padding(2);
             this.tpDeleteTransaction.Name = "tpDeleteTransaction";
             this.tpDeleteTransaction.Padding = new System.Windows.Forms.Padding(2);
-            this.tpDeleteTransaction.Size = new System.Drawing.Size(917, 482);
+            this.tpDeleteTransaction.Size = new System.Drawing.Size(686, 389);
             this.tpDeleteTransaction.TabIndex = 1;
             this.tpDeleteTransaction.Text = "Suppression d\'une transaction";
             this.tpDeleteTransaction.UseVisualStyleBackColor = true;
             // 
             // tpModifTransaction
             // 
-            this.tpModifTransaction.Location = new System.Drawing.Point(4, 28);
+            this.tpModifTransaction.Location = new System.Drawing.Point(4, 25);
+            this.tpModifTransaction.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tpModifTransaction.Name = "tpModifTransaction";
-            this.tpModifTransaction.Size = new System.Drawing.Size(917, 482);
+            this.tpModifTransaction.Size = new System.Drawing.Size(686, 389);
             this.tpModifTransaction.TabIndex = 2;
             this.tpModifTransaction.Text = "Modificationd \'une transaction";
             this.tpModifTransaction.UseVisualStyleBackColor = true;
             // 
             // tpRecap
             // 
-            this.tpRecap.Location = new System.Drawing.Point(4, 28);
+            this.tpRecap.Location = new System.Drawing.Point(4, 25);
+            this.tpRecap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tpRecap.Name = "tpRecap";
-            this.tpRecap.Size = new System.Drawing.Size(917, 482);
+            this.tpRecap.Size = new System.Drawing.Size(686, 389);
             this.tpRecap.TabIndex = 3;
             this.tpRecap.Text = "Récapitulatif";
             this.tpRecap.UseVisualStyleBackColor = true;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(5, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Recette";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
             // frmBudgetMois
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 514);
+            this.ClientSize = new System.Drawing.Size(694, 418);
             this.Controls.Add(this.tcBudgetMois);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -320,8 +296,6 @@
         private System.Windows.Forms.Button btnAjoutType;
         private System.Windows.Forms.ComboBox cbTypeTransa;
         private System.Windows.Forms.Label lblTypeTransa;
-        private System.Windows.Forms.RadioButton rdbPerçu;
-        private System.Windows.Forms.RadioButton rdbRecette;
         private System.Windows.Forms.TextBox txtMontantTransa;
         private System.Windows.Forms.Label lblMontantTransa;
         private System.Windows.Forms.TextBox txtDescriptionTransa;
@@ -332,7 +306,7 @@
         private System.Windows.Forms.GroupBox grpRecettePercu;
         private System.Windows.Forms.TabPage tpModifTransaction;
         private System.Windows.Forms.TabPage tpRecap;
-
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- GestBudget/frmBudgetMois.cs
+++ GestBudget/frmBudgetMois.cs
@@ -25,8 +25,10 @@ namespace GestBudget
         {
             try
             {
-                connec.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Application.StartupPath + "\\budget.mdb";
+                connec.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+Application.StartupPath+"\\budget.mdb";
                 connec.Open();
+                DataTable schema = connec.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                
 
                 string requete = "select count(*) from Poste";
                 OleDbCommand cd1 = new OleDbCommand(requete, connec);
