c370ea70d7825f85fdeeb9e48f467a94680d294b
AntoineCattin <antoine.cattin@free.fr>
2017-06-08 13:23:48 +0200

    MAIRGENT

--- GestBudget/frmBudgetPrevisionnelAnnuel.cs
+++ GestBudget/frmBudgetPrevisionnelAnnuel.cs
@@ -21,30 +21,37 @@ namespace Pique_Sous
         OleDbConnection connec = new OleDbConnection();
         private void frmBudgetPrevisionnelAnnuel_Load(object sender, EventArgs e)
         {
-
+            setupDGV();
         }
 
         public void setupDGV()
         {
+
+            
             //Mise en place de la connection string et on ouvre la connection
             connec.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Application.StartupPath + "\\budget.mdb";
             connec.Open();
 
-            String rqtPoste = "Select libPoste FROM Poste";
-            OleDbCommand cdPoste = new OleDbCommand(rqtPoste, connec);
-            cdPoste.ExecuteNonQuery();
-
-            OleDbDataReader drPoste = cdPoste.ExecuteReader();
-
-            int k = 0;
-
-            if (drPoste.HasRows)
+            try
             {
-                while (drPoste.Read())
-                {
-                    dgvPrevisionnelAnnuel.get = drPoste.GetInt32(k);
-                }
+                String rqtPoste = "SELECT libPoste FROM Poste";
+                OleDbCommand cdPoste = new OleDbCommand(rqtPoste, connec);
+
+                OleDbDataAdapter da = new OleDbDataAdapter(cdPoste);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                dgvPrevisionnelAnnuel.DataSource = dt;
             }
+            catch (InvalidOperationException erreur)
+            {
+                MessageBox.Show("Erreur de chaine de connexion ! DGV \n " + erreur.Message);
+            }
+            catch (OleDbException erreur)
+            {
+                MessageBox.Show("Erreur de requete SQL ! DGV \n " + erreur.Message);
+            }
+
         }
     }
 }
