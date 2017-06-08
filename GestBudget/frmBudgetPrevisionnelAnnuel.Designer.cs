7f6ee4d0c4604e47515b461b5f08761b5f601d1a
AntoineCattin <antoine.cattin@free.fr>
2017-06-08 13:02:32 +0200

    Création colonnes

--- GestBudget/frmBudgetPrevisionnelAnnuel.Designer.cs
+++ GestBudget/frmBudgetPrevisionnelAnnuel.Designer.cs
@@ -28,34 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvPrevisionnelAnnuel = new System.Windows.Forms.DataGridView();
+            this.Poste = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Janvier = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Février = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mars = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Avril = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Juin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Juillet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Août = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Septembre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Novembre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Décembre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrevisionnelAnnuel)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvPrevisionnelAnnuel
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(859, 499);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvPrevisionnelAnnuel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrevisionnelAnnuel.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Poste,
+            this.Janvier,
+            this.Février,
+            this.Mars,
+            this.Avril,
+            this.Mai,
+            this.Juin,
+            this.Juillet,
+            this.Août,
+            this.Septembre,
+            this.Novembre,
+            this.Décembre});
+            this.dgvPrevisionnelAnnuel.Location = new System.Drawing.Point(0, 2);
+            this.dgvPrevisionnelAnnuel.Name = "dgvPrevisionnelAnnuel";
+            this.dgvPrevisionnelAnnuel.RowTemplate.Height = 24;
+            this.dgvPrevisionnelAnnuel.Size = new System.Drawing.Size(859, 499);
+            this.dgvPrevisionnelAnnuel.TabIndex = 0;
+            // 
+            // Poste
+            // 
+            this.Poste.Frozen = true;
+            this.Poste.HeaderText = "Poste : ";
+            this.Poste.Name = "Poste";
+            // 
+            // Janvier
+            // 
+            this.Janvier.HeaderText = "Janvier";
+            this.Janvier.Name = "Janvier";
+            // 
+            // Février
+            // 
+            this.Février.HeaderText = "Février";
+            this.Février.Name = "Février";
+            // 
+            // Mars
+            // 
+            this.Mars.HeaderText = "Mars";
+            this.Mars.Name = "Mars";
+            // 
+            // Avril
+            // 
+            this.Avril.HeaderText = "Avril";
+            this.Avril.Name = "Avril";
+            // 
+            // Mai
+            // 
+            this.Mai.HeaderText = "Mai";
+            this.Mai.Name = "Mai";
+            // 
+            // Juin
+            // 
+            this.Juin.HeaderText = "Juin";
+            this.Juin.Name = "Juin";
+            // 
+            // Juillet
+            // 
+            this.Juillet.HeaderText = "Juillet";
+            this.Juillet.Name = "Juillet";
+            // 
+            // Août
+            // 
+            this.Août.HeaderText = "Août";
+            this.Août.Name = "Août";
+            // 
+            // Septembre
+            // 
+            this.Septembre.HeaderText = "Septembre";
+            this.Septembre.Name = "Septembre";
+            // 
+            // Novembre
+            // 
+            this.Novembre.HeaderText = "Septembre";
+            this.Novembre.Name = "Novembre";
+            // 
+            // Décembre
+            // 
+            this.Décembre.HeaderText = "Décembre";
+            this.Décembre.Name = "Décembre";
             // 
             // frmBudgetPrevisionnelAnnuel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(871, 671);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvPrevisionnelAnnuel);
             this.Name = "frmBudgetPrevisionnelAnnuel";
             this.Text = "frmBudgetPrevisionnelAnnuel";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmBudgetPrevisionnelAnnuel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrevisionnelAnnuel)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvPrevisionnelAnnuel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Poste;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Janvier;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Février;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mars;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Avril;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Juin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Juillet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Août;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Septembre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Novembre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Décembre;
     }
 }
--- GestBudget/frmBudgetPrevisionnelAnnuel.cs
+++ GestBudget/frmBudgetPrevisionnelAnnuel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Pique_Sous
 {
@@ -15,6 +16,35 @@ namespace Pique_Sous
         public frmBudgetPrevisionnelAnnuel()
         {
             InitializeComponent();
+        }
+
+        OleDbConnection connec = new OleDbConnection();
+        private void frmBudgetPrevisionnelAnnuel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void setupDGV()
+        {
+            //Mise en place de la connection string et on ouvre la connection
+            connec.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Application.StartupPath + "\\budget.mdb";
+            connec.Open();
+
+            String rqtPoste = "Select libPoste FROM Poste";
+            OleDbCommand cdPoste = new OleDbCommand(rqtPoste, connec);
+            cdPoste.ExecuteNonQuery();
+
+            OleDbDataReader drPoste = cdPoste.ExecuteReader();
+
+            int k = 0;
+
+            if (drPoste.HasRows)
+            {
+                while (drPoste.Read())
+                {
+                    dgvPrevisionnelAnnuel.get = drPoste.GetInt32(k);
+                }
+            }
         }
     }
 }
