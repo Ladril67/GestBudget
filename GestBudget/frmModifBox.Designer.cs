ca3023b8dc6465627a7a5231e5bc00a36f4c872b
AntoineCattin <antoine.cattin@free.fr>
2017-06-08 13:24:01 +0200

    MAIRGENT

---
+++ GestBudget/frmModifBox.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Pique_Sous
+{
+    partial class frmModifBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmModifBox));
+            this.txtContenu = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContenu
+            // 
+            this.txtContenu.Location = new System.Drawing.Point(64, 29);
+            this.txtContenu.Name = "txtContenu";
+            this.txtContenu.Size = new System.Drawing.Size(200, 22);
+            this.txtContenu.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(148, 17);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Contenu de la cellule :";
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(219, 74);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 2;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(138, 74);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 3;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // frmModifBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 109);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtContenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmModifBox";
+            this.Text = "Modification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContenu;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
